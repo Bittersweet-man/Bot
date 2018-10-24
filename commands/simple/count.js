512d2f2efed6d312945774c90c8d74e828dc19fb
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-10-23 20:50:16 -0700

    +1

--- commands/simple/count.js
+++ commands/simple/count.js
@@ -17,6 +17,9 @@ class CountCommand extends Commando.Command
     {
         if(!message.member.hasPermission("ADMINISTRATOR"))
         message.channel.send("You do not have permission!")
+        var number = 1 
+        number + 1
+        message.channel.send(number + 1)
     }
 }
 
