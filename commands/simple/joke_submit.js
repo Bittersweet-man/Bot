e6942a110b471221abc36e80e0467967f23252d8
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-10-23 19:20:17 -0700

    joke update

--- commands/simple/joke_submit.js
+++ commands/simple/joke_submit.js
@@ -17,7 +17,7 @@ class SubmitCommand extends Commando.Command {
     async run(message, args) {
         message.reply("Yee Haw")
         var channel = message.guild.channels.find("name", "botjokes");
-        channel.send("A joke was just submitted! " + args)
+        channel.send(" if(chance == ){ message.reply(" + args + ")")
     }
 }
 
