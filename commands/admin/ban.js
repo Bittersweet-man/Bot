25844f9d810ec9c448ef767912f403a85f1c3d58
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-10-29 22:14:13 -0700

    ban

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,40 +1,43 @@
 const Commando = require('discord.js-commando');
-
-class BanCommand extends Commando.Command
-{
-    constructor(client,)
-        {
-        super(client,{
+const discord = require('discord.js');
+const bot = new Commando.Client({
+    commandPrefix: '?'
+});
+class BanCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
             name: 'ban',
             group: 'admin',
             memberName: 'ban',
             description: 'Bans a user'
         });
     }
-        
-        
-    async run(message, args)
-    {
-        let bannedUser = message.guild.member(message.mentions.users.first());    
-        if(!bannedUser)
-        {
+
+
+    async run(message, args) {
+        let BannedUser = message.guild.member(message.mentions.users.first());
+        console.log(BannedUser)
+        if (!BannedUser) {
             message.channel.send("Sorry, I cound't find that person");
             return;
-        }   
-        if(!message.member.hasPermission("ADMIN_PERMS"))
-        {
+        }
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send("You don't have permissions to use this command!");
             return;
         }
         let words = args.split(' ');
         let reason = words.slice(1).join(' ');
-        message.guild.member(bannedUser).ban(reason)
-           .then(console.log)
-           .catch(console.error);
-           message.reply("Yee Haw")
+        message.guild.member(BannedUser).ban(reason)
+        message.reply("Yee Haw")
         var channel = message.guild.channels.find("name", "modlogs");
-        channel.send("A user was just banned! Banned User " + bannedUser + " Banned By " + message.author)
+        channel.send("A user was just banned! KBanned User " + BanneddUser + " Banned By " + message.author)
+
+
+
+
     }
 }
 
-module.exports = BanCommand;
+
+
+module.exports = KickCommand;
