dd1f4cc6814ba5f2b5fd80786bd465040a151e3f
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-10-29 20:24:35 -0700

    doggo

---
+++ commands/simple/doggo.js
@@ -0,0 +1,22 @@
+const Commando = require('discord.js-commando');
+
+class DoggoCommand extends Commando.Command
+{
+    constructor(client,)
+        {
+        super(client,{
+            name: 'doggo',
+            group: 'simple',
+            memberName: 'doggo',
+            description: 'doggo'
+        });
+    }
+        
+        
+    async run(message, args)
+    {
+       message.channel.send(`https://random.dog/woof.json`)
+    }
+}
+
+module.exports = CoinFlipCommand;
